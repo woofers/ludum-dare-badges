bdea7f25b31e543ed16f5bc38287786109af5644
Jaxson Van Doorn <woofers1tt@gmail.com>
2019-08-11 17:09:30 -0700

    Stop endpoint if id is invalid

--- src/controllers/large.js
+++ src/controllers/large.js
@@ -6,7 +6,10 @@ const template = dot.template(large)
 
 export default (req, res) => {
   const { id, game } = req.params
-  if (isNaN(id)) res.send('Ludum Dare competition must be a valid integer (eg. `44/game-name`)')
+  if (isNaN(id)) {
+    res.send('Ludum Dare competition must be a valid integer (eg. `44/game-name`)')
+    return
+  }
   svgData(id, game).then(data => {
     res.header("Content-Type", "image/svg+xml")
     res.send(template(data))
