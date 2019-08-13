809a30395d3809cb1c47102aa9875402d73311c6
Jaxson Van Doorn <woofers1tt@gmail.com>
2019-08-13 12:45:54 -0700

    Add support for PNG badges

--- src/routes/large.js
+++ src/routes/large.js
@@ -1,13 +1,26 @@
 import { large as template } from '../svg'
 import { svgData } from '../api'
 import results from '../models/results'
+import { convertToPng } from '../util/png'
+
+const type = (data, rasterize) => {
+  return new Promise((resolve, reject) => {
+    if (!rasterize) return resolve({ data, type: 'image/svg+xml' })
+    convertToPng(data)
+      .then(data => resolve({ data, type: 'image/png' }))
+      .catch(err => reject(err))
+  })
+}
 
 export default (req, res) => {
-  const { id, game } = req.query
+  const { id, game, rasterize } = req.query
   svgData(id, game)
     .catch(err => Promise.resolve(results(err.title, '', err.message)))
-    .then(data => {
-      res.setHeader("Content-Type", "image/svg+xml")
-      res.send(template(data))
+    .then(template)
+    .then(data => type(data, rasterize))
+    .then(img => {
+      res.setHeader('Content-Type', img.type)
+      res.send(img.data)
     })
+    .catch(err => res.send(err))
 }
