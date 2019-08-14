020a110dd81667f47968f2b02d345860201b88c7
Jaxson Van Doorn <woofers1tt@gmail.com>
2019-08-14 13:29:45 -0700

    Prevent N/A superscripts from causing misalignment

--- src/util/ith.js
+++ src/util/ith.js
@@ -3,7 +3,7 @@ export const NA = 'N/A'
 
 // From: https://www.w3resource.com/javascript-exercises/javascript-string-exercise-45.php
 export const ith = number => {
-  if (number === NA) return ''
+  if (number === NA) return '⠀'
   if (number % 100 >= 11 && number % 100 <= 13) return 'th'
   switch (number % 10) {
     case 1: return 'st'
