0e3cbe9b4202fe4df3f9dcc8574df0470ffc85eb
Jaxson Van Doorn <woofers1tt@gmail.com>
2019-08-18 11:41:34 -0700

    Hide URL path on mobile

--- demo/src/pages/index.js
+++ demo/src/pages/index.js
@@ -5,6 +5,13 @@ import Input from '../components/input'
 import Form from '../components/form'
 import Select from '../components/select'
 import Widget from '../components/widget'
+import { css } from '@emotion/core'
+
+const hide = css`
+  @media screen and (max-width: 510px) {
+    display: none;
+  }
+`
 
 const IndexPage = () => {
   const [id, setId] = useState('')
@@ -20,11 +27,11 @@ const IndexPage = () => {
       <h1>Ludum Dare Badges</h1>
       <h3>SVG badges for Ludum Dare Game Jam results</h3>
       <Form>
-        <Input width="200px" disabled label="badges.vandoorn.ca/" />
+        <span css={hide}><Input width="200px" disabled label="badges.vandoorn.ca/" /></span>
         <Input width="145px" label="Ludum Dare #" placeholder="44" type="number" set={setId} />
-        <Input width="35px" disabled label="/" />
+        <span css={hide}><Input width="35px" disabled label="/" /></span>
         <Input width="205px" label="Game" placeholder="alien-e-x-p-a-n-s-i-o-n" set={setName} />
-        <Input width="90px" disabled label="/badge." />
+        <span css={hide}><Input width="90px" disabled label="/badge." /></span>
         <Select options={['svg', 'png']} set={setType} value={type} />
       </Form>
       <Widget game={game} type={type} />
