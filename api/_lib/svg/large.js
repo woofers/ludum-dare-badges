9b41ed9c6becd93aec76aa15692a59d14a5ee342
Jaxson Van Doorn <woofers1tt@gmail.com>
2020-09-04 15:13:17 -0700

    Remove text squish from transform

--- api/_lib/svg/large.js
+++ api/_lib/svg/large.js
@@ -33,7 +33,7 @@ export default it =>
   <text x="25" y="112" font-size="24" fill="#d0d0d8" font-weight="bold">${sanitize(it.game)}</text>
 
   <!-- Error Message -->
-  <text transform="scale(0.85, 1); translate(6, 0)" x="25" y="140" font-size="16px" font-weight="300" fill="#d0d0d8"><tspan>${it.error || ''}</tspan></text>
+  <text transform="translate(1, 0)" x="25" y="140" font-size="16px" font-weight="300" fill="#d0d0d8"><tspan>${it.error || ''}</tspan></text>
 
   <!-- Logo -->
   ${!it.error ? '<a xlink:href="' + it.link + '" target="_blank" rel="noopener noreferrer" xlink:title="View Ludum Dare Page">' : ''}
@@ -62,7 +62,7 @@ export default it =>
   ${it.error ? '-->' : ''}
 
   <!-- Categories -->
-  <text transform="scale(0.85, 1); translate(10, 0)" x="61" y="126" font-size="16px" fill="#d0d0d8">
+  <text transform="translate(10, 0)" x="61" y="126" font-size="16px" fill="#d0d0d8">
     <tspan font-weight="300" x="61" dy="1.63em">${sanitize(it.stats[0].category)}</tspan>
     <tspan font-weight="300" x="61" dy="1.63em">${sanitize(it.stats[1].category)}</tspan>
     <tspan font-weight="300" x="61" dy="1.63em">${sanitize(it.stats[2].category)}</tspan>
@@ -94,7 +94,7 @@ export default it =>
   </text>
 
   <!-- Ratings -->
-  <text transform="scale(0.85, 1); translate(50, 0)" x="207" y="126" font-size="16px" fill="#d0d0d8">
+  <text transform="translate(50, 0)" x="207" y="126" font-size="16px" fill="#d0d0d8">
     <tspan font-weight="300" x="207" dy="1.63em">${sanitize(it.stats[0].average)}</tspan>
     <tspan font-weight="300" x="207" dy="1.63em">${sanitize(it.stats[1].average)}</tspan>
     <tspan font-weight="300" x="207" dy="1.63em">${sanitize(it.stats[2].average)}</tspan>
