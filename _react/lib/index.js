55833c8d10ab5b17156e7a934594d7dc257340fd
Jaxson Van Doorn <woofers1tt@gmail.com>
2023-02-05 19:32:37 +0000

    Bump to v2.1.2

--- _react/lib/index.js
+++ _react/lib/index.js
@@ -1,6 +1,6 @@
 if (process.env.NODE_ENV !== 'production') {
-    module.exports = require('./ludum-dare-badges.dev.cjs')
+    module.exports = require('./ludum-dare-badges.dev.js')
   }
   else {
-    module.exports = require('./ludum-dare-badges.cjs')
+    module.exports = require('./ludum-dare-badges.js')
   }
