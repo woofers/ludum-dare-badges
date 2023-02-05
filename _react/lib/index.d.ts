9806fa04c64fa127d8d36a789476d7929a01b056
Jaxson Van Doorn <woofers1tt@gmail.com>
2023-02-05 19:22:42 +0000

    Bump react package version

--- _react/lib/index.d.ts
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-type Props = {
-    host?: string;
-    game: string;
-    type?: 'svg' | 'png';
-};
-declare const LudumDare: React.FC<Props>;
-export default LudumDare;
--- _react/lib/index.module.js
+++ _react/lib/index.module.js
@@ -1,5 +1,5 @@
-import WidgetDev from './ludum-dare-badges.modules.dev.js'
-import WidgetProd from './ludum-dare-badges.modules.js'
+import WidgetDev from './ludum-dare-badges.module.dev.js'
+import WidgetProd from './ludum-dare-badges.module.js'
 
 const Widget = process.env.NODE_ENV === 'production' ? WidgetProd : WidgetDev
 
