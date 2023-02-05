7687984c3e8dd24811264448656e5c0ad1708306
Jaxson Van Doorn <woofers1tt@gmail.com>
2023-02-05 19:20:25 +0000

    Add index TypeScript file

---
+++ _react/lib/index.d.ts
@@ -0,0 +1,8 @@
+import React from 'react';
+type Props = {
+    host?: string;
+    game: string;
+    type?: 'svg' | 'png';
+};
+declare const LudumDare: React.FC<Props>;
+export default LudumDare;
