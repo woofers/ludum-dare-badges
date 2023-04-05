b801492a71f24f9b260c7b835b7300456e3fb302
Jaxson Van Doorn <woofers1tt@gmail.com>
2023-04-05 03:35:05 +0000

    Use path resolve

--- lib/util/png.ts
+++ lib/util/png.ts
@@ -1,11 +1,16 @@
 import svg2img from 'svg2img'
+import { join, resolve as pathResolve } from 'path'
 
 export const convertToPng = (svg: string) => {
   return new Promise((resolve, reject) => {
     svg2img(svg, {
       resvg: {
         font: {
-          fontFiles: ['./fonts/Roboto/Roboto-Light.ttf', './fonts/Roboto/Roboto-Bold.ttf', './fonts/Raleway/Raleway-ExtraBold.ttf'],
+          fontFiles: [
+            join(pathResolve('.'), 'fonts', 'Roboto', 'Roboto-Light.ttf'),
+            join(pathResolve('.'), 'fonts', 'Roboto', 'Roboto-Bold.ttf'),
+            join(pathResolve('.'), 'fonts', 'Raleway', 'Raleway-ExtraBold.ttf'),
+          ],
           loadSystemFonts: false, 
           defaultFontFamily: 'Roboto'
         }
